123ea6d643a50bf5553561a72da8f35b8041f205
Json <wjs330183@163.com>
2020-04-28 12:01:03 +0800

    update definition.md

---
+++ 100Classic_C_LanguageInterviewQuestions/Definition/2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <windows.h>
+typedef int (*max2)(int x, int y);
+/**
+*typedef的功能是定义新的类型。
+第一句就是定义了一种 pfun 的类型，并定义这种类型为指向某种函数的指针，这种函数以一个 int 为参数并返回 void 类型。
+*/
+int max1(int x, int y)
+{
+    return x > y ? x : y;
+}
+main()
+{
+    int a, b, c;
+    scanf("%d %d", &a, &b);
+    max2 p_max = max1;
+    c = p_max(a, b);
+    printf("%d \n", c);
+    system("pause");
+}
